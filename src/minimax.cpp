ee6f306fb3bde95a91fb42f1f46bcdf3ad2cd0d8
Sameer Javed Ahmad <sjahmad@ghc34.ghc.andrew.cmu.edu>
2019-12-07 18:32:36 -0500

    Created Minimax Signature

--- src/minimax.cpp
+++ src/minimax.cpp
@@ -1,1 +1,11 @@
-adasd
+#include "board.h"
+#include "minimax.h"
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <cstring>			// Remove the unnecessary ones later //
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
--- src/minimax.h
+++ src/minimax.h
@@ -1,1 +1,42 @@
-sdad
+#ifndef MINIMAX_H
+#define MINIMAX_H
+
+#include <math.h>
+
+class Pos
+{
+public:
+	int row;
+	int col;
+	Pos() = default;
+    Pos(int vrow, int vcol)
+    {
+        row = vrow;
+        col = vcol;
+    }	
+};
+
+class Move 
+{
+public:
+	Pos old;
+	Pos new;
+};
+
+class MiniMax
+{
+public:
+    // std::vector<Particle> particles;
+    // std::vector<Particle> newParticles;
+    // std::unique_ptr<INBodySimulator> nbodySimulator;
+
+    // List of variables that need to be global across class (if any)
+
+    float Evaluate(ChessBoard board, int player);
+
+    Move Generate_Next(ChessBoard board, int depth);
+
+
+};
+
+#endif
