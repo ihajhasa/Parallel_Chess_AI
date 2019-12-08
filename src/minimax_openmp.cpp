cec55eb9190b475fb162b625f09608746e368375
Ishaq Yousef Haj Hasan <ihajhasa@andrew.cmu.edu>
2019-12-08 17:08:00 -0500

    still

--- src/minimax_openmp.cpp
+++ src/minimax_openmp.cpp
@@ -67,7 +67,6 @@ Best_Move parallel_max(ChessBoard board, int depth, Move move, int color)
 		moves_score[i] = parallel_max(board, depth-1, mv, (color != WHITE) ? WHITE : BLACK);
 
 		bc.free_board();
-		free(&bc);
 	}
 
 	Best_Move max_score = fast_get_max(moves_score, 0, moves_score.size());
@@ -128,7 +127,6 @@ Best_Move parallel_min(ChessBoard board, int depth, Move move, int color)
 		moves_score[i] = parallel_max(board, depth-1, mv, (color != WHITE) ? WHITE : BLACK);
 
 		bc.free_board();
-		free(&bc);
 	}
 
 	Best_Move min_score = fast_get_min(moves_score, 0, moves_score.size());
