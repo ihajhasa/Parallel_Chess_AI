f7cc41f635c4bb78be62e16472d9296b4dd042a5
Ishaq Yousef Haj Hasan <ihajhasa@andrew.cmu.edu>
2019-12-08 13:03:12 -0500

    worked on game.cpp

--- src/Test Files/game.cpp
+++ src/Test Files/game.cpp
@@ -2,6 +2,52 @@
 #include "../board_rep.h"
 #include "../minimax.h"
 
+void print_piece(Piece *p_p)
+{
+    if(p_p == NULL) 
+    {
+        std::cout << "Emp";
+        return;
+    }
+
+
+    Piece p = *p_p;
+
+    if (p.color == BLACK)
+        std::cout << "B";
+    else if (p.color == WHITE)
+        std::cout << "W";
+
+    if (p.name == PAWN)
+        std::cout << "Pa";
+    else if (p.name == ROOK)
+        std::cout << "Ro";
+    else if (p.name == KNIGHT)
+        std::cout << "Kn";
+    else if (p.name == BISHOP)
+        std::cout << "Bi";
+    else if (p.name == QUEEN)
+        std::cout << "Qu";
+    else if (p.name == KING)
+        std::cout << "Ki";
+}
+
+void print_board(ChessBoard B)
+{
+    std::cout << "-----------------------------------------" << std::endl;
+    for(int row = 0; row < 8; row++)
+    {
+        for(int col = 0; col < 8; col++)
+        {
+            std::cout << "| ";
+            print_piece(B.lookup(row, col));
+        }
+        std::cout << "|" << std::endl;
+        std::cout << "-----------------------------------------" << std::endl;
+    }
+    std::cout << std::endl << std::endl;
+}
+
 int is_valid_move(ChessBoard *B, int color, int oldrow, int oldcol, int newrow, int newcol)
 {
     Piece *p, *tmp;
@@ -27,13 +73,13 @@ void process_user(ChessBoard *B, int color)
     int oldrow, oldcol, newrow, newcol;
 
     std::cout << "Enter Current Piece Row [0-7] (up to down)" << std::endl;
-    cin >> oldrow;
+    std::cin >> oldrow;
     std::cout << "Enter Current Piece Column [0-7] (left to right)" << std::endl;
-    cin >> oldcol;
+    std::cin >> oldcol;
     std::cout << "Enter New Piece Row [0-7] (up to down)" << std::endl;
-    cin >> newrow;
+    std::cin >> newrow;
     std::cout << "Enter New Piece Column [0-7] (left to right)" << std::endl;
-    cin >> newcol;
+    std::cin >> newcol;
 
     if(is_valid_move(B, color, oldrow, oldcol, newrow, newcol))
     {
@@ -46,13 +92,15 @@ void process_user(ChessBoard *B, int color)
     }
 }
 
-void process_AI(ChessBoard *B, int color, int depth)
+void process_AI(ChessBoard *B, MiniMax AI, int color, int depth)
 {
     Move nextmove;
 
-    nextmove = Generate_Next(*B, depth, color);
+    nextmove = AI.Generate_Next(*B, depth, color);
 
-    (*B).move(oldrow, oldcol, newrow, newcol);
+    (*B).move(nextmove.Old.row, nextmove.Old.col, nextmove.New.row, nextmove.New.col);
+    std::cout << "Moved (" << nextmove.Old.row << ", " << nextmove.Old.col << ") to (" << nextmove.New.row << ", " << nextmove.New.col << ")" << std::endl;
+    
 }
 
 
@@ -64,6 +112,7 @@ int main (int argc, char** argv) {
 
     int is_AI_white = 1, is_AI_black = 1;
     int AI_depth = 5;
+    MiniMax AI_White, AI_Black;
 
     for(int i = 1; i < argc; i++)
     {
@@ -84,6 +133,8 @@ int main (int argc, char** argv) {
     ChessBoard *B = new ChessBoard;
     (*B).init();
 
+    print_board(*B);
+
     if(is_AI_white) (*B).set_AI_color(WHITE);
     if(is_AI_black) (*B).set_AI_color(BLACK);
 
@@ -96,13 +147,15 @@ int main (int argc, char** argv) {
         if(turn == WHITE)
         {
             if(!is_AI_white) process_user(B, turn);
-            else process_AI(B, turn, AI_depth);
+            else process_AI(B, AI_White, turn, AI_depth);
         }
         else if (turn == BLACK)
         {
             if(!is_AI_black) process_user(B, turn);
-            else process_AI(B, turn, AI_depth);
+            else process_AI(B, AI_Black, turn, AI_depth);
         }
+
+        print_board(*B);
 
         turn = (turn != WHITE) ? WHITE : BLACK;
 
